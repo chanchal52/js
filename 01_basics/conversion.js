981fd3ec1b2bdb0a30e998f336e2024cedb9bc9c
unknown <chanchalrgh121@gmail.com>
2025-03-13 15:54:03 +0530

    conversions

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -1,5 +1,11 @@
 let score="chanchal"
-console.log(typeof score);
+//console.log(typeof score);
 valueInNumber=Number(score) 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//OPERATIONS
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
