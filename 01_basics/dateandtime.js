8bcaeb033b478b870e75ed808dd1a7c481abc689
unknown <chanchalrgh121@gmail.com>
2025-03-16 12:39:32 +0530

    date and time

---
+++ 01_basics/dateandtime.js
@@ -0,0 +1,13 @@
+let myDate=new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+
+let mycd=new Date(2023,0,24)
+// console.log(mycd.toString())
+let my=new Date(2025,3,2,4,5,3)
+// console.log(my.toLocaleString())
+
+let mycredate=new Date("2025-01-13")
+// console.log(mycredate.toLocaleString())
