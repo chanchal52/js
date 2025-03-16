8c8fe13d6f7d7582449e1fad817dbe345a8a7aac
unknown <chanchalrgh121@gmail.com>
2025-03-16 11:47:56 +0530

    maths

---
+++ 01_basics/numbermaths.js
@@ -0,0 +1,23 @@
+const score =40
+// console.log(score);
+const num= new Number(100)
+// console.log(num)
+//console.log(num.toString().length)
+// console.log(num.toFixed(1))
+
+const otherno=23.896
+// console.log(otherno.toPrecision(3))
+
+const other=123.8966
+// console.log(other.toPrecision(3))
+
+const hundreds=1000000000
+// console.log(hundreds.toLocaleString())
+// console.log(hundreds.toLocaleString('en-IN'))
+
+console.log(Math.abs(-4))
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1)+min))
