21ca9d928c823e8e2ec514df32de23a8e36e7fe4
unknown <chanchalrgh121@gmail.com>
2025-03-18 22:18:08 +0530

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -49,12 +49,44 @@
 // console.log(loginusername())
 
 //**we can also set a default value
-function loginusername(username="sam"){
-    if (username=== undefined) {
-        console.log("please enter valid username")
-        return
-    }
+// function loginusername(username="sam"){
+//     if (username=== undefined) {
+//         console.log("please enter valid username")
+//         return
+//     }
         
-    return `${username} just logged in`
+//     return `${username} just logged in`
+// }
+// console.log(loginusername())
+// function calculatecartprice(n1){
+//     return n1
+// }
+// console.log(calculatecartprice(200))
+// function calculatecartprice(...n1){
+//     return n1
+// }
+// console.log(calculatecartprice(20,30,40)) //agr multiple values pass krni ho tw (...n1) aise likho...just like spread...
+//spread hee hai isme isko flat bolte
+function calculatecartprice(val1,val2,...n1){
+    return n1
 }
-console.log(loginusername())
+// console.log(calculatecartprice(20,30,40,50)) //val1 20,val2-30 fr n1 40 50 retyrn krega
+const user={
+    username:"hitesh",
+    price:19
+}
+function handleobject(anyobject){
+    // console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+// handleobject(user) //abhi object bna k pass kiye the
+//direct object bhi pass kr skte hai like below
+handleobject({
+    username:"sam",price:10
+})
+//** ab array ka krke dekhte hai ..functions aur object ko pass krke dekhe
+const mynewarray=[20,40,60]
+function returnsecondelement(getarray){
+    return getarray[1]
+}
+// console.log(returnsecondelement(mynewarray)) 
+console.log(returnsecondelement([20,40,60])) //direct array passs kr rhe h 
